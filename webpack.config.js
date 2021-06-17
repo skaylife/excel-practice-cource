663e06402daaa4177ee22ed86fd1fbdf61d32224
SkayPC <worldskaynet@gmail.com>
2021-06-17 16:20:44 +0700

    create webpack fix HASH

--- webpack.config.js
+++ webpack.config.js
@@ -1,12 +1,11 @@
-const { node } = require("webpack");
-const path = require('path');
+const path = require('path')
 
-node.exports = {
+module.exports = {
   context: path.resolve(__dirname, 'src'),
   mode: 'development',
-  entry: '.index.js',
+  entry: './index.js',
   output: {
-    filename: 'bundele.js',
+    filename: 'bundle.[hash].js',
     path: path.resolve(__dirname, 'dist')
   }
 }
